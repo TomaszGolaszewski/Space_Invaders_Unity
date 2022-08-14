12d4ed740c7656fcb4550fbce5db219d2fba9235
Tomasz Gołaszewski <golaszewski.t@gmail.com>
2022-08-14 11:10:03 +0200

    Commit no. 6

--- Space_Invaders_Unity/Assets/Scripts/ScoreTable.cs
+++ Space_Invaders_Unity/Assets/Scripts/ScoreTable.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ScoreTable : MonoBehaviour
 {
     private Transform entryContainer;
     private Transform entryTemplate;
 
+    //void Start()
     private void Awake()
     {
         entryContainer = transform.Find("ScoreEntryContainer");
@@ -14,15 +17,29 @@ public class ScoreTable : MonoBehaviour
 
         entryTemplate.gameObject.SetActive(false);
 
-        float templateHeight = 20f;
+        float templateHeight = 80f;
         for (int i = 0; i < 10; i++)
-        {
+        {     
             Transform entryTransform = Instantiate(entryTemplate, entryContainer);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
             entryTransform.gameObject.SetActive(true);
-        }
 
+            //Debug.Log("test4");
+
+            int rank = i + 1;
+            string rankString;
+            switch (rank)
+            {
+                default:
+                    rankString = rank + "TH"; break;
+                case 1: rankString = "1ST"; break;
+                case 2: rankString = "2ND"; break;
+                case 3: rankString = "3RD"; break;
+            }
+            entryTransform.Find("Text_Pos").GetComponent<TextMeshProUGUI>().text = rankString;
+           // entryTransform.Find("Text_test").GetComponent<Text>().text = rankString;          
+        }
     }
 
     /*
